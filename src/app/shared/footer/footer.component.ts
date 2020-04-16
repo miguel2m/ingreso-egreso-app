42045f8c0e880dfaa3cc532995e39044304054da
miguelangel <miguelangelmedina2494@gmail.com>
2020-04-16 18:19:38 -0400

    Arreglando Footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class FooterComponent implements OnInit {
-
+  fecha:number;
   constructor() { }
 
   ngOnInit(): void {
+    this.fecha = new Date().getFullYear();
   }
 
 }
